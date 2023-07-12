da48d0ec3053f754b17ea9988db388204df6a502
Sergey <stroitelevsergeyalex@gmail.com>
2023-07-12 22:36:24 +0300

    commit hw2.8

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -4,17 +4,26 @@ type ActionType =
     | { type: 'sort'; payload: 'up' | 'down' }
     | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => { // need to fix any
     switch (action.type) {
         case 'sort': { // by name
-
-            return state // need to fix
+            let copyState = [...state]
+            copyState.sort((a, b) => {
+                if (action.payload === 'up') {
+                    return (a.name.toLowerCase() > b.name.toLowerCase()) ? 1 : -1
+                } else if (action.payload === 'down') {
+                    return  (a.name.toLowerCase() < b.name.toLowerCase()) ? 1 : -1
+                } else {
+                    return 0
+                }
+            })
+            return copyState // need to fix
         }
         case 'check': {
-
-            return state // need to fix
+            return state.filter(v => v.age > 18) // need to fix
         }
         default:
             return state
     }
 }
+
